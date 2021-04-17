fa725e48e45c0ba2dd92ef0979c871da25b864f8
kennyjijet <usistal@hotmail.com>
2021-04-17 11:05:24 +0700

    added initiative UI for LOPA.

--- react/src/page/LOPA.tsx
+++ react/src/page/LOPA.tsx
@@ -3,9 +3,13 @@ import React from "react";
 export default function LOPA(props: any) {
     const { component } = props;
     return (
-        <div>
-            <div>
-                XXX
+        <div style={{width: "100%"}}>
+            <div style={{overflow: "hidden", margin: "0px auto", width: "600px", height:"100vh", background: "red"}}>
+                <div style={{height: "20%", background: "gray"}}></div>
+                <div style={{overflowX: "hidden", overflowY: "scroll", height: "70%", background: "green"}}>
+                    <div style={{height: "1000px", background: "cyan"}}></div>
+                </div>
+                <div style={{height: "10%", background: "blue"}}></div>
             </div>
         </div>
     );
